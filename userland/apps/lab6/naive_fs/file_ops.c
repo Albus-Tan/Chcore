50359b4a4ce909f9c1e6e56dbf90eb16befd757c
Albus-Tan <tzm0425@163.com>
2023-05-13 02:06:06 +0800

    60/80

--- userland/apps/lab6/naive_fs/file_ops.c
+++ userland/apps/lab6/naive_fs/file_ops.c
@@ -4,11 +4,45 @@
 #include "file_ops.h"
 #include "block_layer.h"
 
+#define ENTRY_SIZE 32
+#define ENTRY_NUM BLOCK_SIZE/ENTRY_SIZE
+#define NAME_BLOCK_IDX 0
+#define FIRST_BLOCK_IDX NAME_BLOCK_IDX+1
+#define END_LABEL '$'
+
+// Use the first block to store name and block index
+// name$
+
+static int next_idx = FIRST_BLOCK_IDX;
+
 int naive_fs_access(const char *name)
 {
     /* LAB 6 TODO BEGIN */
     /* BLANK BEGIN */
 
+    // read first block which store name
+    char buf[BLOCK_SIZE];
+    sd_bread(NAME_BLOCK_IDX, buf);
+    size_t len = strlen(name);
+
+    // traverse all entry
+    for(int i = 0; i < ENTRY_NUM; ++i){
+            // start from i * ENTRY_SIZE
+            int j;
+            for(j = 0; j < len; ++j){
+                    if(buf[i*ENTRY_SIZE + j] != name[j] || buf[i*ENTRY_SIZE + j] == END_LABEL){
+                            // check next entry
+                            break;
+                    }
+            }
+            if(j == len && buf[i*ENTRY_SIZE + j] == END_LABEL){
+                    // has entry
+                    // printf("entry %d match\n", i);
+                    return 0;
+            }
+            // printf("entry %d unmatch\n", i);
+    }
+    return -1;
     /* BLANK END */
     /* LAB 6 TODO END */
     return -2;
@@ -18,6 +52,30 @@ int naive_fs_creat(const char *name)
 {
     /* LAB 6 TODO BEGIN */
     /* BLANK BEGIN */
+    if(naive_fs_access(name) == 0){
+            return -1;
+    }
+
+    // read first block which store name
+    char buf[BLOCK_SIZE];
+    sd_bread(NAME_BLOCK_IDX, buf);
+    size_t len = strlen(name);
+
+    // alloc block
+    int idx = next_idx;
+    ++next_idx;
+
+    // add name into first block
+    strcpy(buf + idx*ENTRY_SIZE, name);
+    buf[idx*ENTRY_SIZE + len] = END_LABEL;
+
+    // printf("file named %s added\n", name);
+    // printf("buf: %s\n", buf);
+
+    // update block which store name
+    sd_bwrite(NAME_BLOCK_IDX, buf);
+
+    return 0;
 
     /* BLANK END */
     /* LAB 6 TODO END */
