d897b135737e3e7a0d8cf62232d53e126cf4ded4
Albus-Tan <tzm0425@163.com>
2023-05-13 02:20:23 +0800

    finish 80/80

--- userland/apps/lab6/naive_fs/file_ops.c
+++ userland/apps/lab6/naive_fs/file_ops.c
@@ -15,6 +15,33 @@
 
 static int next_idx = FIRST_BLOCK_IDX;
 
+int get_file_block(const char *name)
+{
+        // read first block which store name
+        char buf[BLOCK_SIZE];
+        sd_bread(NAME_BLOCK_IDX, buf);
+        size_t len = strlen(name);
+
+        // traverse all entry
+        for(int i = 0; i < ENTRY_NUM; ++i){
+                // start from i * ENTRY_SIZE
+                int j;
+                for(j = 0; j < len; ++j){
+                        if(buf[i*ENTRY_SIZE + j] != name[j] || buf[i*ENTRY_SIZE + j] == END_LABEL){
+                                // check next entry
+                                break;
+                        }
+                }
+                if(j == len && buf[i*ENTRY_SIZE + j] == END_LABEL){
+                        // has entry
+                        // printf("entry %d match\n", i);
+                        return i;
+                }
+                // printf("entry %d unmatch\n", i);
+        }
+        return -1;
+}
+
 int naive_fs_access(const char *name)
 {
     /* LAB 6 TODO BEGIN */
@@ -86,6 +113,18 @@ int naive_fs_pread(const char *name, int offset, int size, char *buffer)
 {
     /* LAB 6 TODO BEGIN */
     /* BLANK BEGIN */
+    int blk = get_file_block(name);
+    if(blk == -1){
+            return -1;
+    }
+
+    // read file block
+    char buf[BLOCK_SIZE];
+    sd_bread(blk, buf);
+
+    memcpy(buffer, buf+offset, size);
+
+    return size;
 
     /* BLANK END */
     /* LAB 6 TODO END */
@@ -96,6 +135,21 @@ int naive_fs_pwrite(const char *name, int offset, int size, const char *buffer)
 {
     /* LAB 6 TODO BEGIN */
     /* BLANK BEGIN */
+    int blk = get_file_block(name);
+    if(blk == -1){
+            return -1;
+    }
+
+    // read file block
+    char buf[BLOCK_SIZE];
+    sd_bread(blk, buf);
+
+    memcpy(buf+offset, buffer, size);
+
+    // write file block
+    sd_bwrite(blk, buf);
+
+    return size;
 
     /* BLANK END */
     /* LAB 6 TODO END */
@@ -106,6 +160,9 @@ int naive_fs_unlink(const char *name)
 {
     /* LAB 6 TODO BEGIN */
     /* BLANK BEGIN */
+    if(naive_fs_access(name) != 0){
+            return -1;
+    }
 
     /* BLANK END */
     /* LAB 6 TODO END */
